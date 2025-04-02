93ca5a2a82283fdfa5861785f87591ed91514f9f
Amit Upadhyay <greencode1002@gmail.com>
2025-04-02 13:22:03 -0400

    fixed history query

--- models/Patient.js
+++ models/Patient.js
@@ -4,9 +4,10 @@ const Patient = {
     getPatientHistory: (id, callback) => {
         const sql = `SELECT u.firstname, u.lastname,u.email, ps.symptom_id, 
         ps.patient_id, ps.age, ps.nurse_id, ps.is_prescribed, 
-        p.medicine,p.dosage,p.notes,p.is_payment_completed FROM users u, 
-        patients_symptoms ps, prescriptions p WHERE u.id = ps.patient_id 
-        and ps.symptom_id = p.symptom_id and u.id = ?`;
+        p.medicine,p.dosage,p.notes,p.is_payment_completed 
+        FROM users u left join patients_symptoms ps 
+        ON u.id = ps.patient_id left outer join prescriptions p 
+        ON ps.symptom_id = p.symptom_id where u.id = ?`;
         db.query(sql, [id], callback);
     },
 };
