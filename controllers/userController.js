f49d4b0f4c7cb0845b7f525f05e61ed4f51f0e37
Amit Upadhyay <greencode1002@gmail.com>
2025-04-03 00:09:31 -0400

    fixed users crud

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,6 +10,15 @@ exports.register = (req, res) => {
     });
 };
 
+exports.updateUser = (req, res) => {
+    const { firstname, lastname, email, password, role } = req.body;
+    const { id } = req?.params;
+    User.update({ id, firstname, lastname, email, password, role }, (err, result) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.status(201).json({ message: "User registered successfully" });
+    });
+}
+
 exports.login = (req, res) => {
     const { email, password } = req.body;
     User.findByEmail(email, async (err, results) => {
@@ -24,7 +33,7 @@ exports.login = (req, res) => {
     });
 };
 
-// New getAllUsers function
+
 exports.getAllUsers = (req, res) => {
     User.getAllUsers((err, results) => {
         if (err) return res.status(500).json({ error: err.message });
@@ -38,5 +47,13 @@ exports.getUsersByRole = (req, res) => {
         if (err) return res.status(500).json({ error: err.message });
         if (results.length === 0) return res.status(404).json({ message: `No users found with the role ${role}` });
         res.status(200).json({ users: results });
+    });
+};
+
+exports.deleteUser = (req, res) => {
+    const { id } = req.params;
+    User.deleteUser(id, (err, results) => {
+        if (err) return res.status(500).json({ error: err.message });
+        res.json({ message: "User deleted successfully" });
     });
 };
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -35,6 +35,41 @@ userRouter.post("/register", User.register);
 
 /**
  * @swagger
+ * /api/users/updateuser/{id}:
+ *   post:
+ *     summary: Update user
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstname:
+ *                 type: string
+ *               lastname:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *               role:
+ *                 type: string
+ *                 enum: ["doctor", "nurse", "patient"]
+ *     responses:
+ *       201:
+ *         description: User updated successfully
+ */
+userRouter.put("/updateuser/:id", User.updateUser);
+
+/**
+ * @swagger
  * /api/users/login:
  *   post:
  *     summary: Login user
@@ -85,5 +120,22 @@ userRouter.get("/", User.getAllUsers);
  *         description: List of users with the specified role
  */
 userRouter.get("/role/:role", User.getUsersByRole);
+
+/**
+ * @swagger
+ * /api/users/delete/{id}:
+ *   delete:
+ *     summary: Delete a user
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: User deleted successfully
+ */
+userRouter.delete("/delete/:id", User.deleteUser);
 
 module.exports = userRouter;
